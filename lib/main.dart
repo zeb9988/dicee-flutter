9dd62d32876ea2a7c6c9e7c57e5ca087aa251070
zeb9988 <kzeb6788@gmail.com>
2022-07-27 11:34:31 +0500

    initialize commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
-        ),
-        body: DicePage(),
-      ),
-    ),
-  );
+  return runApp(const DicePage());
 }
 
 class DicePage extends StatelessWidget {
+  const DicePage({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: const Text('Dicee'),
+          backgroundColor: Colors.black,
+        ),
+        body: const DicePage(),
+      ),
+    );
   }
 }
